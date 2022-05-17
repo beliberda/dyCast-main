f90d1e4a827f6806b040d7ccb5ab5cc01b8b2d7f
Anton <anton.nurgaliev@mail.ru>
2022-05-17 18:11:02 +1000

    add videoprew

--- src/components/CoubBackground/index.jsx
+++ src/components/CoubBackground/index.jsx
@@ -7,10 +7,20 @@ import Small_2 from "../../assets/RoundCube-Blue-small-2.png";
 import Small_3 from "../../assets/RoundCube-Blue-small-3.png";
 import Vector_Group from "../../assets/Vector-group-1.svg";
 import Vector_Group_2 from "../../assets/Vector-group-2.svg";
+import videoprew1 from "../../assets/VideoPrew1.png";
+import videoprew2 from "../../assets/VideoPrew2.png";
+import videoprew3 from "../../assets/VideoPrew3.png";
+import videoprew4 from "../../assets/VideoPrew4.png";
+import videoprew5 from "../../assets/VideoPrew5.png";
 export default function CoubBack() {
   return (
    <div className="background">
      <img src={BigCoube} alt="" className="big-coube" />
+     <img src={videoprew1} alt="" className="videoprew-1" />
+     <img src={videoprew2} alt="" className="videoprew-2" />
+     <img src={videoprew3} alt="" className="videoprew-3" />
+     <img src={videoprew4} alt="" className="videoprew-4" />
+     <img src={videoprew5} alt="" className="videoprew-5" />
      <img src={Vector_Group} alt="" className="Vector-group" />
      <img src={Vector_Group_2} alt="" className="Vector-group-2" />
      <img src={Small_1} alt="" className="small-coube" />
